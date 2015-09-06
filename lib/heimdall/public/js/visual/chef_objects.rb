190d607d87011a4e42019aa88560b69e401dcc1d
Douglas Triggs <doug@chef.io>
2015-09-06 12:31:17 -0600

    Added top-level chef server view

--- lib/heimdall/public/js/visual/chef_objects.rb
+++ lib/heimdall/public/js/visual/chef_objects.rb
@@ -1,20 +1,19 @@
 # Register system scripts
 
+# Toplevel display
+Heimdall.store.register('chef-server', "visual 'chef-server' do; show_json; end")
+
 # Simple list-item elements
 ['chef-client', 'chef-cookbook', 'chef-data-bag', 'chef-environment', 'chef-node',
  'chef-role', 'chef-user'].each do |endpoint|
-  Heimdall.store.register(endpoint, "visual '#{endpoint}' do; show_json; end")
-  Heimdall.store.register("#{endpoint}-list",
-                          "visual '#{endpoint}-list' do; show_json; end")
+  Heimdall.store.register(endpoint, "visual '#{endpoint}' do; end")
+  Heimdall.store.register("#{endpoint}-list", "visual '#{endpoint}-list' do; end")
 end
 
 # Special chef items
-Heimdall.store.register("chef-data-bag-item",
-                        "visual 'chef-data-bag-item' do; show_json; end")
-Heimdall.store.register("chef-search",
-                        "visual 'chef-search' do; show_json; end")
-Heimdall.store.register("chef-run-list-item",
-                        "visual 'chef-run-list-item' do; show_json; end")
+Heimdall.store.register('chef-data-bag-item', "visual 'chef-data-bag-item' do; end")
+Heimdall.store.register('chef-search', "visual 'chef-search' do; end")
+Heimdall.store.register('chef-run-list-item', "visual 'chef-run-list-item' do; end")
 
 # TODO: Role: env_run_lists?
 # TODO: Add stack : breadcrumbs to Chef Viewer
